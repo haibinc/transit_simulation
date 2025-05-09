f1ef9e372795652bc275ba04b93881bd6668aec7
Enviousnes <danield0322@yahoo.com>
2025-05-09 14:09:31 -0700

    added passenger class

--- Bus.cpp
+++ Bus.cpp
@@ -21,3 +21,33 @@ void Bus::reportStop() const
 {
     std::cout << "Bus #" << busNumber << " is at stop " << currentStop << " on route " << route->getRouteName() << std::endl;
 }
+
+std::vector<Passenger*> Bus::getPassengers() const { return passengers; }
+
+int Bus::getPassengerCount() const { return passengers.size(); }
+
+bool Bus::isFull() const { return passengers.size() >= getTotalCapacity(); }
+
+bool Bus::addPassenger(Passenger* passenger)
+{
+    if (!isFull())
+    {
+        passengers.push_back(passenger);
+        addPassengers(1);
+        return true;
+    }
+    return false;
+}
+
+bool Bus::removePassenger(Passenger* passenger)
+{
+    std::vector<Passenger*>::iterator it = std::remove(passengers.begin(), passengers.end(), passenger);
+    if (it != passengers.end())
+    {
+        passengers.erase(it);
+        deletePassengers(1);
+        return true;
+    }
+    return false;
+}
+
